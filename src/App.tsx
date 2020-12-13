b17e5aabb0d560e986be2022173a883a2f1581b2
zpears <zachary.p.pearson@gmail.com>
2020-12-12 21:38:57 -0500

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import MainGame from './views/MainGame';
+import './App.css';
+
+export default function App() {
+  return (
+    <div className="App">
+      <MainGame />
+    </div>
+  );
+}
---
+++ src/views/MainGame.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function MainGame() {
+  return (
+    <div><h1>The Main Game</h1></div>
+  )
+}
