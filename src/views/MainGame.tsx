daa9a7b2752262f9b6631ff4aebc6ba89f427749
zpears <zachary.p.pearson@gmail.com>
2020-12-12 22:20:23 -0500

    showing keycodes

--- src/views/MainGame.tsx
+++ src/views/MainGame.tsx
@@ -1,7 +1,22 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 export default function MainGame() {
+
+  const [keyCode, setKeyCode] = useState<number>(0);
+
+  const handleKeyDown = (event: KeyboardEvent) => {
+    setKeyCode(event.keyCode);
+  }
+
+  useEffect(() => {
+    window.addEventListener('keydown', handleKeyDown);
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown);
+    }
+  }, []);
+
   return (
-    <div><h1>The Main Game</h1></div>
+    <div><h1>The Main Game</h1>
+      <p>The pressed keycode: {keyCode}</p></div>
   )
 }
